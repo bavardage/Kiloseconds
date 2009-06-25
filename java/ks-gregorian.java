2d3488106b8b1cb6b7d4ac7398d7a678c294e728
jelly <jellevdwaa@gmail.com>
2009-06-26 00:55:02 +0200

    java: the implementation of kiloseconds
    Signed-off-by: jelly <jellevdwaa@gmail.com>

---
+++ java/ks-gregorian.java
@@ -0,0 +1,19 @@
+import java.util.GregorianCalendar;
+
+public class Kiloseconds {
+
+	public static void main(String[] args) {
+		
+		GregorianCalendar calendar = new GregorianCalendar();
+		int hours, minutes, seconds, kiloseconds;
+		
+		hours = calendar.get(GregorianCalendar.HOUR_OF_DAY);
+		minutes = calendar.get(GregorianCalendar.MINUTE);
+		seconds = calendar.get(GregorianCalendar.SECOND);
+		kiloseconds = (hours*3600 + minutes*60 + seconds) / 1000;
+		System.out.println(kiloseconds + "\n");
+
+	}
+
+}
+
