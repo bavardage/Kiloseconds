f859b179e17459be5c3b02146a0f2b9dd9fac1d3
Ben Duffield <jebavarde@gmail.com>
2009-06-26 09:48:18 +0100

    Added clojure from wiki
    Signed-off-by: Ben Duffield <jebavarde@gmail.com>

---
+++ clojure/ks.clj
@@ -0,0 +1,14 @@
+(import '(java.util Calendar))
+
+(defn kiloseconds
+  "Current time in kiloseconds."
+  []
+  (let [calendar (Calendar/getInstance)
+        hours (. calendar (get Calendar/HOUR_OF_DAY))
+        minutes (. calendar (get Calendar/MINUTE))
+        seconds (. calendar (get Calendar/SECOND))
+        kiloseconds (/ (+ (* hours 3600) (* minutes 60) seconds) 1000)]
+    (println (str "Kiloseconds: " (float kiloseconds)))))
+
+;; call it                                                                      
+(kiloseconds)
