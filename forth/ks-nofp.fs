6f9c3f9d5837dcc12dfdb92621f398e28f0c3cfc
tlvb <insomninja@gmail.com>
2009-09-04 00:39:25 +0800

    changed to scj's suggested printing function
    Signed-off-by: tlvb <insomninja@gmail.com>

--- forth/ks-nofp.fs
+++ forth/ks-nofp.fs
@@ -1,7 +1,7 @@
 \ Forth example without floating point division (but instead a special printing function).
 \ Tested with gforth 64 bit.
 
-: dk. ( n -- ) s>d swap over dabs <<# # # # [char] . hold #s rot sign #> TYPE SPACE #>> ;
+: dk. s>d <# # # # '. hold #s #> type ;
 : seconds ( -- n ) time&date 2drop drop 60 * + 60 * + ;
 : printkstime ( -- ) ." It is " seconds dk. ." kiloseconds." cr ;
 
