5b7b46455268f0254c8c5c187539c7506fde9aa5
kampsun <kampsuniahv@gmail.com>
2009-11-19 03:18:49 +0800

    Fortran version

---
+++ fortran/ks.f95
@@ -0,0 +1,13 @@
+program Kiloseconds
+    
+    integer,dimension(8) :: date
+    real :: ks
+
+    call date_and_time(VALUES=date) ! VALUE(5) - hour,
+                                    ! VALUE(6) - minutes,
+                                    ! VALUE(7) - seconds,
+                                    ! VALUE(8) - milliseconds.
+    ks = date(5)*3.6 + date(6)*0.06 + date(7)*0.001 + date(8)*10**(-6)
+
+    write(*,'(F6.3)'), ks
+end program Kiloseconds
